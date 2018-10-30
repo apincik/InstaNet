d479009118d62a5ea6ad6d33da3a6d0317a0ae94
Andrej Pincik <apincik@gmail.com>
2018-10-30 01:25:53 +0100

    add include proxy extensions

---
+++ include/driver-extensions/background.js
@@ -0,0 +1,28 @@
+var config = {
+    mode: "fixed_servers",
+    rules: {
+      singleProxy: {
+        scheme: "http",
+        host: "%IP_ADDRESS%",
+        port: parseInt("%PORT%")
+      },
+      bypassList: ["foobar.com"]
+    }
+  };
+
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "%USERNAME%",
+            password: "%PASSWORD%"
+        }
+    };
+}
+
+chrome.webRequest.onAuthRequired.addListener(
+        callbackFn,
+        {urls: ["<all_urls>"]},
+        ['blocking']
+);
